5e51d864e6c1c5d22f1b5e1c1c02796922e143b9
Michael Halim <michaelhalimsenatra@gmail.com>
2025-12-11 22:45:29 +0700

    Fix Next js Config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-}
+  poweredByHeader: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
